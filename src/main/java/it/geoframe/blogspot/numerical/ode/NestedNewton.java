6d39fa651ee97c938f76b018e2b35ea519bab916
niccolotubini <tubini.niccolo@gmail.com>
2021-09-05 13:51:13 +0200

    Add the interface NonLinearEquationSolver

--- src/main/java/it/geoframe/blogspot/numerical/ode/NestedNewton.java
+++ src/main/java/it/geoframe/blogspot/numerical/ode/NestedNewton.java
@@ -24,7 +24,7 @@ package it.geoframe.blogspot.numerical.ode;
  * @author Niccolò Tubini, Giuseppe Formetta
  * 
  */
-public class NestedNewton {
+public class NestedNewton implements NonLinearEquationSolver {
 
 	private double x0;
 	
--- src/main/java/it/geoframe/blogspot/numerical/ode/NonLinearEquationSolver.java
+++ src/main/java/it/geoframe/blogspot/numerical/ode/NonLinearEquationSolver.java
@@ -24,51 +24,9 @@ package it.geoframe.blogspot.numerical.ode;
  * @author Niccolò Tubini, Giuseppe Formetta
  * 
  */
-public class NewtonRaphson implements NonLinearEquationSolver {
+public interface NonLinearEquationSolver {
 
-	private double x0;
 	
-	private double xNew;
-	
-	private double tol = 1e-10;
-	
-	private double error;
-	
-	private int iteration;
-	
-	private int maxIteration = 100;
-	
-	
-	
-	public double solve( double x, OrdinaryDifferentialEquation ode ) {
-		
-		this.x0 = x;
-		
-		error = 1.0;
-		
-		iteration = 1;
-		
-		
-		while(error>tol) {
-		
-			if(iteration>maxIteration) {
-				
-				System.out.println("\t NewtonRaphson reach maximum numeber of iteration " + ode.toString());
-				break;
-				
-			}
-			
-			xNew = -ode.compute(this.x0)/ode.computeDerivative(this.x0) + this.x0;
-			
-			error = Math.abs(xNew-this.x0)/this.x0;
-			
-			this.x0 = xNew;
-			
-			iteration++;
-				
-		}
-		
-		return xNew;
-	}
+	public double solve( double x, OrdinaryDifferentialEquation ode );
 	
 }
